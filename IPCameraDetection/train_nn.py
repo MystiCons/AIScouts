08476cc49fd90d41694e3ade5f1cdea8d24893ec
EppuHeilimo <eppu.heilimo@yahoo.com>
2017-07-05 17:05:58 +0300

    Add new model

--- IPCameraDetection/train_nn.py
+++ IPCameraDetection/train_nn.py
@@ -2,6 +2,6 @@ from model import Model
 
 paths = ['/Car/', '/Park/']
 data_folder = "/media/cf2017/levy/tensorflow/parking_place/"
-mod = Model(paths, learning_rate=0.001, layers=3, epochs=10,
-           data_folder=data_folder, model_name='park_model' + str(19))
-mod.train_model()
+mod = Model(paths, learning_rate=0.001, layers=4, epochs=10,
+           data_folder=data_folder, model_name='park_model' + str(20))
+mod.train_model(saved_train_data_path=data_folder + "park_model19/")
