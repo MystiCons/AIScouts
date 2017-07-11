d35afd6e28888a32e9d2ae683cbae4ac55909ec7
EppuHeilimo <eppu.heilimo@yahoo.com>
2017-07-11 09:48:54 +0300

    Add new model

--- Utils/vpn/vpn-check.sh
+++ Utils/vpn/vpn-check.sh
@@ -2,8 +2,11 @@
 ping -c3 192.168.51.140 > pingreport
 result="$(ifconfig ppp0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"
 if [[ $result != *'192'* ]]; then
-echo shit
 pon labranet
 sleep 10
 python3 /home/pi/dev/AIScouts/RaspberryVersion/send_ppp_address.py
+fi
+result="$(pgrep -fl rasp_run_detection.py)"
+if [[ $result != *'python3'* ]]; then
+python3 /home/pi/dev/AIScouts/RaspberryVersion/rasp_run_detection.py
 fi
